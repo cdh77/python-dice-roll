d3deb20bd6c45c8e16ba586a9776dafaab4b3354
cdh77 <ultra1omega1@gmail.com>
2020-07-29 14:43:28 +0530

    Completed Interactive Dice Roll

--- dice_roller.py
+++ dice_roller.py
@@ -1,11 +1,17 @@
 def main():
   import random
-  dice_rolls = 2
-  dice_sum = 0 
-  for i in range(0, dice_rolls):
-    roll = random.randint(1, 6)
+  dice_rolls = int(input('How many dice would you like to roll? '))
+  dice_sum = 0
+  dice_size = int(input('How many sides are the dice? '))
+  for i in range(0,dice_rolls):
+    roll = random.randint(1,dice_size)
     dice_sum += roll
-    print(f'You rolled a {roll}')
+    if roll == 1:
+      print(f'You rolled a {roll}! Critical Fail')
+    elif roll == dice_size:
+      print(f'You rolled a {roll}! Critical Success!')
+    else:
+      print(f'You rolled a {roll}')
   print(f'You have rolled a total of {dice_sum}')
 
 if __name__== "__main__":
